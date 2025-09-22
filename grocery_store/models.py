a3fac2c10db3e19bb9649abe2fd166403c64e1d9
Haleem Akintayo <127564674+haleemakintayo@users.noreply.github.com>
2025-09-22 14:32:53 +0100

    added updates

--- grocery_store/models.py
+++ grocery_store/models.py
@@ -1,11 +1,35 @@
 from django.db import models
 from django.urls import reverse
+from django.utils.text import slugify
 
 # Create your models here.
+
+def generate_unique_slug(instance, value_field='name', slug_field='slug'):
+    """
+    Generate a unique slug for `instance` based on `value_field`.
+    Works by slugifying the value and appending a counter if needed.
+    """
+    base = slugify(getattr(instance, value_field))
+    slug = base
+    ModelClass = instance.__class__
+    counter = 1
+
+    # Build query for existing slugs (exclude current instance when updating)
+    while True:
+        lookup = {slug_field: slug}
+        qs = ModelClass.objects.filter(**lookup)
+        if instance.pk:
+            qs = qs.exclude(pk=instance.pk)
+        if not qs.exists():
+            return slug
+        slug = f"{base}-{counter}"
+        counter += 1
+
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
-    slug = models.SlugField(unique=True,max_length=200)
+    slug = models.SlugField(unique=True,max_length=200, null=True, blank=True)
 
     class Meta:
         verbose_name = 'Category'
@@ -14,6 +38,11 @@ class Category(models.Model):
     def __str__(self):
         return self.name
     
+    def save(self, *args, **kwargs):
+        if not self.slug and self.name:
+            self.slug = generate_unique_slug(self, value_field='name', slug_field='slug')
+        super().save(*args, **kwargs)
+
     def get_absolute_url(self):
         return reverse('product_list_by_category', args=[self.slug])
     
@@ -21,7 +50,7 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
-    slug = models.SlugField(unique=True, max_length=200)
+    slug = models.SlugField(unique=True, max_length=200,null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     sale_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     stock = models.PositiveIntegerField(default=0)
@@ -31,10 +60,18 @@ class Product(models.Model):
     is_featured = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    unit = models.CharField(max_length=50, blank=True, null=True)  # e.g., 'kg', 'liters', 'pcs'
 
 
     def __str__(self):
         return self.name
+    
+    def save(self, *args, **kwargs):
+        if not self.slug and self.name:
+            self.slug = generate_unique_slug(self, value_field='name', slug_field='slug')
+        super().save(*args, **kwargs)
+
+
     def get_absolute_url(self):
         return reverse('product_detail', args=[self.slug])
 
--- grocery_store/views.py
+++ grocery_store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from.models import Product, Category
 
 def home(request):
     return HttpResponse("Hello, this is my new app!")
@@ -14,6 +15,9 @@ def about(request):
 def contact(request):
     return render(request, 'contact.html')
 def shop(request):
-    return render(request, 'shop.html')
-def product_detail(request):
-    return render(request, 'product_details.html')
+    categories = Category.objects.all()
+    products = Product.objects.all()
+    return render(request, 'shop.html', {'categories': categories, 'products': products})
+def product_detail(request, slug):
+    product = Product.objects.get(slug=slug)
+    return render(request, 'product_details.html', {'product': product})
